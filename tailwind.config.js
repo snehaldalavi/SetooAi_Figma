c5462b142a8f2fa926795a69dbf1384d7b3d4f0b
snehaldalavi <145430340+snehaldalavi@users.noreply.github.com>
2024-12-10 19:36:00 +0530

    Add files via upload
    SetooAi_oncall figma page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+       fontFamily : {
+        sans : ['Plus Jakarta Sans' , 'sans-serif'],
+        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+       },
+    },
+  },
+  plugins: [],
+}
