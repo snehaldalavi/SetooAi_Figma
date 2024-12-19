2a53e3b88fee6968ad5c4b24c83633296bd42e69
snehaldalavi <145430340+snehaldalavi@users.noreply.github.com>
2024-12-19 19:16:52 +0530

    Add files via upload
    Date 19/12/24
    
    uploaded changes made till this date

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
         sans : ['Plus Jakarta Sans' , 'sans-serif'],
         jakarta: ['Plus Jakarta Sans', 'sans-serif'],
        },
+        boxShadow :{
+          'md' : '0px 4px 16px 0px rgba(239, 84, 73, 0.10)'
+        },
+        gridTemplateColumns: {
+          
+          'custom': '327px auto',
+        },
+        fill : {
+          'white' : '#ffffff',
+        },
     },
+    
   },
+varients : {
+  fill : ['hover','focus'],
+},
+
   plugins: [],
 }
