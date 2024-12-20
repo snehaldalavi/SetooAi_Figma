5199aa75eafb3588505ae23d1e5ff5bf19260bfe
snehaldalavi <145430340+snehaldalavi@users.noreply.github.com>
2024-12-20 19:12:20 +0530

    Add files via upload
    created new pages and fixed issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
         jakarta: ['Plus Jakarta Sans', 'sans-serif'],
        },
         boxShadow :{
-          'md' : '0px 4px 16px 0px rgba(239, 84, 73, 0.10)'
+          'md' : '0px 4px 16px 0px rgba(239, 84, 73, 0.10)',
+          'gray' : '0px 4px 4px rgba(0, 0, 0, 0.25)',
         },
         gridTemplateColumns: {
           
           'custom': '327px auto',
+          'custom1' : '90.997px auto',
         },
+        
         fill : {
           'white' : '#ffffff',
         },
